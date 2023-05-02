a88233a91aa1ceb401f761eadc3cb7a60227a7fd
IhorChepak <chepak_lex@ukr.net>
2023-05-02 23:29:08 +0300

    done

--- src/js/index.js
+++ src/js/index.js
@@ -8,67 +8,61 @@ const loadMoreBtn = document.querySelector('button.load-more');
 
 form.addEventListener('submit', onSubmit);
 loadMoreBtn.addEventListener('click', onLoadMore);
-// let queriesArray = [];
 
 function onSubmit(e) {
-  e.preventDefault();
-  loadMoreBtn.classList.add('is-hidden');
-  gallery.innerHTML = '';
-  imageApiService.query = e.currentTarget.elements.searchQuery.value.trim();
-  imageApiService.resetPage();
-  if (imageApiService.query === '') {
-    Notify.info('Please enter your search query!');
-    return;
-  } else {
-    imageApiService
-      .getImage()
-      .then(data => {
-        let queriesArray = data.hits;
-        if (queriesArray.length === 0) {
-          Notify.failure(
-            'Sorry, there are no images matching your search query. Please try again.'
-          );
-        } else if (queriesArray.length < 40) {
-          renderImages(queriesArray);
-          loadMoreBtn.classList.add('is-hidden');
-          Notify.success(`Hooray! We found ${data.totalHits} images.`);
-          // Notify.info(
-          //   "We're sorry, but you've reached the end of search results."
-          // );
-        } else {
-          renderImages(queriesArray);
-          Notify.success(`Hooray! We found ${data.totalHits} images.`);
-          loadMoreBtn.classList.remove('is-hidden');
-        }
-      })
-      .catch(error => {
-        Notify.info(
-          "We're sorry, but you've reached the end of search results."
-        );
-        console.log(error);
-      });
-  }
+    e.preventDefault();
+    loadMoreBtn.classList.add('is-hidden');
+    gallery.innerHTML = '';
+    imageApiService.query = e.currentTarget.elements.searchQuery.value.trim();
+    imageApiService.resetPage();
+    if (imageApiService.query === '') {
+        Notify.info('Please enter your search query!');
+        return;
+    } else {
+        imageApiService
+            .getImage()
+            .then(data => {
+                let queriesArray = data.hits;
+                if (queriesArray.length === 0) {
+                    Notify.failure(
+                        'Sorry, there are no images matching your search query. Please try again.'
+                    );
+                } else if (queriesArray.length < 40) {
+                    renderImages(queriesArray);
+                    loadMoreBtn.classList.add('is-hidden');
+                    Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+                } else {
+                    renderImages(queriesArray);
+                    Notify.success(`Hooray! We found ${data.totalHits} images.`);
+                    loadMoreBtn.classList.remove('is-hidden');
+                }
+            })
+            .catch(error => {
+                Notify.info(
+                    "We're sorry, but you've reached the end of search results."
+                );
+                console.log(error);
+            });
+    }
 }
 
 function onLoadMore() {
-  imageApiService.getImage().then(data => {
-    let queriesArray = data.hits;
-    renderImages(queriesArray);
-    if (queriesArray.length < 40) {
-      loadMoreBtn.classList.add('is-hidden');
-      Notify.info("We're sorry, but you've reached the end of search results.");
-      // gallery.insertAdjacentHTML(
-      //   'afterend',
-      //   `<p>We're sorry, but you've reached the end of search results.</p>`
-      // );
-    }
-  });
+    imageApiService.getImage().then(data => {
+        let queriesArray = data.hits;
+        renderImages(queriesArray);
+        if (queriesArray.length < 40) {
+            loadMoreBtn.classList.add('is-hidden');
+            Notify.info("We're sorry, but you've reached the end of search results.");
+
+        }
+    });
 }
 
 function renderImages(queriesArray) {
-  const markup = queriesArray
-    .map(item => {
-      return `<div class="photo-card">
+    const markup = queriesArray
+        .map(item => {
+            return `<div class="photo-card">
   <div class="thumb"><img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" /></div>
   <div class="info">
     <p class="info-item">
@@ -85,7 +79,7 @@ function renderImages(queriesArray) {
     </p>
   </div>
 </div>`;
-    })
-    .join('');
-  gallery.insertAdjacentHTML('beforeend', markup);
+        })
+        .join('');
+    gallery.insertAdjacentHTML('beforeend', markup);
 }
